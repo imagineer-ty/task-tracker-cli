3078bae45486a3f5305125e4b290a29015fc939f
Ty <tyrone.brown5311@gmail.com>
2026-02-24 18:44:23 -0500

    added delete and update changes for tasks

--- task-tracker-cli/task_cli.py
+++ task-tracker-cli/task_cli.py
@@ -25,12 +25,35 @@ def add_task(description):
     tasks = load_tasks()
     new_id = max([task["id"] for task in tasks], default=0) + 1
     task = {
-        "id": new_id,
-        "description": description
+            "id": new_id,
+            "description": description,
+            "status": "todo"
     }
     tasks.append(task)
     save_tasks(tasks)
     print(f"Task added successfully (ID: {new_id})")
+
+#--- Delete tasks ---
+def delete_task(task_id):
+    tasks = load_tasks()
+    new_tasks = [task for task in tasks if task["id"] != task_id]
+
+    if len(new_tasks) == len(tasks):
+        print("Task ID not found.")
+        return
+
+    save_tasks(new_tasks) 
+    print(f"task {task_id} deleted successfully")
+
+def mark_done(task_id):
+    tasks = load_tasks()
+    for task in tasks:
+        if task["id"] == task_id:
+            task["status"] = "done"
+            save_tasks(tasks)
+            print(f"Task {task_id} marked as done.")
+            return
+    print("Task ID not found.")
 
 # --- List all tasks ---
 def list_tasks():
@@ -41,18 +64,61 @@ def list_tasks():
     for task in tasks:
         print(f"ID: {task['id']} - Description: {task['description']}")
 
+def update_task(task_id, new_description):
+    tasks = load_tasks()
+    for task in tasks:
+        if task["id"] == task_id:
+            task["description"] = new_description
+            save_tasks(tasks)
+            print(f"Task {task_id} updated successfully.")
+            return
+    print("Task ID not found.")
+
 # --- CLI argument handling ---
 if len(sys.argv) < 2:
-    print("Please provide a command: add or list")
+    print("Please provide a command: add, list, delete")
+
 else:
     command = sys.argv[1]
+
     if command == "add":
         if len(sys.argv) < 3:
             print("Please provide a task description.")
         else:
             description = sys.argv[2]
             add_task(description)
+
     elif command == "list":
         list_tasks()
+
+    elif command == "delete":
+        if len(sys.argv) < 3:
+            print("Please provide a task ID.")
+        else:
+            try:
+                task_id = int(sys.argv[2])
+                delete_task(task_id)
+            except ValueError:
+                print("Task ID must be a number.")
+
+    elif command == "update":
+        if len(sys.argv) < 4:
+            print("Usage: update <id> <new description>")
+        else:
+            try:
+                task_id = int(sys.argv[2])
+                new_desc = sys.argv[3]
+                update_task(task_id, new_desc)
+            except ValueError:
+                 print("Task ID must be a number.")
+    elif command == "mark-done":
+        if len(sys.argv) < 3:
+            print("Please provide a task ID.")
+        else:
+            try:
+                task_id = int(sys.argv[2])
+                mark_done(task_id)
+            except ValueError:
+                print("Task ID must be a number.")
     else:
-        print("Unknown command. Use 'add' or 'list'.")
+        print("Unknown command. Use 'add', 'list', or 'delete'.")
